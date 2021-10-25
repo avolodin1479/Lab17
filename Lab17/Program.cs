a9e8f8059086372a3732e3c7f05cffa343133e57
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-25 12:25:19 +0600

    Добавьте файлы проекта.

---
+++ Lab17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class Accaunt<T>
+        {
+            private T number;
+            private double balance;
+            private string owner;
+        }
+        static void Input<T>(T number, double balance, string owner)
+        {
+            Console.WriteLine("Номер счета:");
+            number = ;
+
+            Console.WriteLine("Баланс:");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ФИО владельца:");
+            owner = Console.ReadLine();
+
+        }
+        static void OutPut<T>(T number, double balance, string owner)
+        {
+            Console.WriteLine("Номер счета {0}", number);
+            Console.WriteLine("Баланс {0}", balance);
+            Console.WriteLine("Владелец {0}", owner);
+        }
+
+    }
+}
